0eb6926a5d3c418bf9182f695cc002e1d87b6e44
DamianJ <sabybna@gmail.com>
2024-04-27 21:13:18 -0300

    url updates

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -1,11 +1,12 @@
 import  axios  from 'axios'
+import { URL_API } from '../../config';
 
 
 
 export const getCartByUser = async ({userNow,cart,erasePrev}) => {
   const token = localStorage.getItem('auth-token-app')
     try {
-        const resp = await axios.post('http://localhost:8080/api/user/setorder', {cart:cart,user: userNow,erasePrev:erasePrev},
+        const resp = await axios.post(URL_API+'/api/user/setorder', {cart:cart,user: userNow,erasePrev:erasePrev},
         {headers: {'Authorization': token}});
         return resp
         }
@@ -18,7 +19,7 @@ export const getStoredByUser = async () => {
       const user = JSON.parse(atob(token.split(".")[1])).user
   
       try {
-          const resp = await axios.post('http://localhost:8080/api/user/getorder', {user: user},
+          const resp = await axios.post(URL_API+'/api/user/getorder', {user: user},
           {headers: {'Authorization': token}});
           return resp
           }
@@ -29,7 +30,7 @@ export const getStoredByUser = async () => {
 export async function hasStore(user){
   const token = localStorage.getItem('auth-token-app')
     try {
-      const resp = await axios.post('http://localhost:8080/api/user/countorder', {user: user},
+      const resp = await axios.post(URL_API+'/api/user/countorder', {user: user},
       {headers: {'Authorization': token}});
       return resp
     } catch (error) {
@@ -39,7 +40,7 @@ export async function hasStore(user){
 export async function cartToSales({user,cart}){
   const token = localStorage.getItem('auth-token-app')
     try {
-      const resp = await axios.post('http://localhost:8080/api/user/orderToSales',
+      const resp = await axios.post(URL_API+'/api/user/orderToSales',
       {user:user,cart:cart,email:'null'},
      {headers: {'Authorization': token}});   
       return resp;
